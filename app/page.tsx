37c8ecad8c3a19908b137916876b6e91ec9ce12b
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-29 22:43:37 +0000

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to My AWS Amplify App</h1>
+      <p>This is a Next.js app deployed on AWS Amplify.</p>
+    </main>
+  );
+}
