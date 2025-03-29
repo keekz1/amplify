be734f87058372cd58e455955300696e5952c655
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-29 23:56:14 +0000

    test amplify

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
+// Add ESLint exception for global declaration
+// eslint-disable-next-line no-var
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
