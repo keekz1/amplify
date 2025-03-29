083ee4092342c39bba0a533bae28af427569a7b2
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-29 22:56:44 +0000

    test amplify

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
+
+import{Poppins} from "next/font/google";
+import{cn} from "@/lib/utils";
+import { Button } from "../components/ui/button";
+//import { LoginButton } from "../components/auth/login-button";
+
+const font = Poppins({
+  subsets:["latin"],
+  weight:["600"]
+
+})
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to My AWS Amplify App</h1>
-      <p>This is a Next.js app deployed on AWS Amplify.</p>
+    <main className="flex h-screen flex-col items-center justify-center"
+      style={{
+        background: "radial-gradient(ellipse at top, #38bdf8, #1e40af)",
+      }}
+    >
+<div className="space-y-6 text-center">
+  <h1 className={cn(
+  "text-6xl font-semibold text-white drop-shadow-md",
+  font.className,
+  )}>
+🔐Auth
+  </h1>
+  <p className="text-white text-lg">
+    A simple authentications service
+  </p>
+  <div>
+    <Button variant="secondary" size="lg">
+       Sign in
+     </Button>
+
+  
+  </div>
+
+</div>
     </main>
   );
 }
