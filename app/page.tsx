4a9f29a573df0038e15ddc9b292f4451dde6fd5b
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-29 23:01:30 +0000

    test amplify

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import{Poppins} from "next/font/google";
 import{cn} from "@/lib/utils";
 import { Button } from "../components/ui/button";
-//import { LoginButton } from "../components/auth/login-button";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
   subsets:["latin"],
@@ -27,9 +27,11 @@ export default function Home() {
     A simple authentications service
   </p>
   <div>
+    <LoginButton >
     <Button variant="secondary" size="lg">
        Sign in
      </Button>
+    </LoginButton>
 
   
   </div>
