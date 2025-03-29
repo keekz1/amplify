924320cdb416836aa30600196e23845669d87a1d
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-29 23:48:43 +0000

    test amplify

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,14 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-
+// Declare the global variable for Prisma Client
 declare global {
-
-var prisma: PrismaClient  |  undefined ;
-
+  var prisma: PrismaClient | undefined;
 }
 
-
-
-
+// Initialize the Prisma Client
 export const db = globalThis.prisma || new PrismaClient();
 
-
-
-if(process.env.NODE_ENV !== "production") globalThis.prisma = db ;
+// Store Prisma Client in the global object during development
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = db;
+}
