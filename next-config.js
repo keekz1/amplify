615b97a3d0d96663de0e9ae02df2ac38d8a069c0
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-30 03:21:56 +0100

    changes to fix memory issue AWS

--- next-config.js
+++ next-config.js
@@ -1,17 +1,47 @@
 // next.config.js
-module.exports = {
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+module.exports = (phase) => {
+  const isProdBuild = phase === PHASE_PRODUCTION_BUILD;
+  
+  return {
     swcMinify: true,
+    compress: true,
     experimental: {
       optimizeCss: true,
-      modularizeImports: {
-        '@prisma/client': {
-          transform: '@prisma/client/{{member}}',
-          skipDefaultConversion: true,
-        },
-      },
+      swcTraceProfiling: isProdBuild,
+      memoryWorkers: true,
+      workerThreads: false,
+      optimizePackageImports: [
+        '@prisma/client',
+        'react-icons'
+      ],
     },
-    webpack: (config) => {
-      config.externals = [...config.externals, '@prisma/client'];
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = { 
+          ...config.resolve.fallback,
+          fs: false,
+          tls: false,
+          net: false,
+          perf_hooks: false
+        };
+      }
+      
+      config.externals.push({
+        '@prisma/client': 'commonjs2 @prisma/client',
+        'prisma': 'commonjs2 prisma'
+      });
+
+      config.cache = {
+        type: 'filesystem',
+        maxMemoryGenerations: 1,
+        buildDependencies: {
+          config: [__filename]
+        }
+      };
+
       return config;
-    },
+    }
   };
+};
