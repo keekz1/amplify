d913a6ba6dae4c2f7d79a360b8e8720d4ba275f6
keekz1 <148965858+keekz1@users.noreply.github.com>
2025-03-30 02:45:58 +0100

    test amplify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
